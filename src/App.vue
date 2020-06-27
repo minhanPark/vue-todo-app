3ba8ba5ccdfcb69ea7ebb6ee3fa532538f2066e9
minhan <alsgks1218@gmail.com>
2020-06-28 00:59:59 +0900

    todo app initial setting

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <TodoHeader></TodoHeader>
+    <TodoInput></TodoInput>
+    <TodoList></TodoList>
+    <TodoFooter></TodoFooter>
+  </div>
+</template>
+
+<script>
+import TodoHeader from "../src/components/TodoHeader";
+import TodoInput from "../src/components/TodoInput";
+import TodoList from "../src/components/TodoList";
+import TodoFooter from "../src/components/TodoFooter";
+
+export default {
+  components: {
+    TodoHeader: TodoHeader,
+    TodoInput: TodoInput,
+    TodoList: TodoList,
+    TodoFooter: TodoFooter
+  }
+};
+</script>
+
+<style>
+</style>
